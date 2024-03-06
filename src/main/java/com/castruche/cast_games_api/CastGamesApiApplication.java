bb6786413b97d3632c4780b488d3931e7ef9f108
Castruche <aurelien.blot@gmail.com>
2024-03-06 14:48:02 +0100

    first commit

---
+++ src/main/java/com/castruche/cast_games_api/CastGamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.castruche.cast_games_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastGamesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastGamesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castruche/cast_games_api/CastGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castruche.cast_games_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastGamesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
